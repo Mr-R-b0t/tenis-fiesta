7aff3a166d49a91c3cde1926b3b2d64ee5a95129
Sébastien <52678042+Mr-R-b0t@users.noreply.github.com>
2023-10-09 21:55:56 +0200

    API

--- back/server.js
+++ back/server.js
@@ -1,12 +1,35 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
-const port = 3001;
+const cookieParser = require("cookie-parser");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const corsOption = {
+  credentials: true, //authetication cookies
+  origin: "https://web.toxicsed.fr",
+};
+
+app.use(cookieParser());
+app.use(cors(corsOption));
+
+//models
+const db = require("./app/models");
+db.connex.sync();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "welcome" });
 });
+//router
+require("./app/routes/product.route")(app);
+require("./app/routes/user.route")(app);
+require("./app/routes/checkout.route")(app);
+require("./app/routes/order.route")(app);
+require("./app/routes/wishlist.route")(app);
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+const PORT = 8008;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-

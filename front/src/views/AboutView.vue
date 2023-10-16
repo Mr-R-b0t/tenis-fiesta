301c6a2906907eeefaba084399552465c927afed
Adam N'MILY <80126560+RawLukki@users.noreply.github.com>
2023-10-16 17:12:36 +0200

    update about us

--- front/src/views/AboutView.vue
+++ front/src/views/AboutView.vue
@@ -1,8 +1,25 @@
 <template>
   <div class="about">
-      <div class="header d-flex align-items-center">
-         <h1>This is the about page</h1>
+      <div class="header">
+        <h1 class="text-center">Our brand</h1>
       </div>
+
+      <div class="header">
+        <h1 class="text-center">Our team</h1>
+        <p class="">Our team is composed of best programer on the market :</p>
+        <ul>
+          <li>MOREL Rebecca</li>
+          <li>PESTANA Sebastian</li>
+          <li>N'MILY Adam</li>
+          <li>MEYNET Sébastian</li>
+        </ul>
+        <p>Join us on social medias to have access to the next news !</p>
+        <div class="fluid">
+        <img src="../assets/instagram.png" alt="Instagram" class="image">
+        <img src="../assets/logo_x.png" alt="Instagram" class="image">
+        </div>
+      </div>
+
     </div>
   </template>
 
@@ -10,8 +27,18 @@
   .about{
     position: relative;
     top:0px;
+    padding-top: 140px;
   }
   .header{
     height: 400px;
+    border: solid 2px black;
+    padding:5px;
+    margin-bottom: 10px;
+  }
+
+    .image{
+    height: 70px;
+    width: 70px;
+    margin: 10px;
   }
   </style>

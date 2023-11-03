bfaf21c26ee7c527f7ce7afb43ae511ff31eac4b
Sébastien <52678042+Mr-R-b0t@users.noreply.github.com>
2023-11-03 14:43:06 +0100

    update backend
    Adding JSDOC and major update on backend

--- back/app/controllers/checkout.controller.js
+++ back/app/controllers/checkout.controller.js
@@ -2,94 +2,129 @@ const db = require("../models");
 const CheckOut = db.checkout;
 const OP = db.Sequelize.Op;
 
+/**
+ * Find and return all checkouts from the database.
+ *
+ * @param {import('express').Request} req - The Express request object.
+ * @param {import('express').Response} res - The Express response object.
+ */
 exports.findAll = (req, res) => {
-    db.checkout.findAll().then((data) => {
-        res.send(data);
-    });
+  CheckOut.findAll().then((data) => {
+    res.send(data);
+  });
 };
 
+/**
+ * Create a new checkout entry in the database.
+ *
+ * @param {import('express').Request} req - The Express request object.
+ * @param {import('express').Response} res - The Express response object.
+ */
 exports.create = (req, res) => {
-    console.log(req.body);
-    CheckOut.create(req.body)
+  console.log(req.body);
+  CheckOut.create(req.body)
     .then((data) => {
-        res.send(data);
+      res.send(data);
     })
     .catch((e) => {
-        res.status(500).send({
-            message: "Could not insert into the table"
-        });
+      res.status(500).send({
+        message: "Could not insert into the table",
+      });
     });
 };
 
+/**
+ * Find a checkout entry by its ID.
+ *
+ * @param {import('express').Request} req - The Express request object.
+ * @param {import('express').Response} res - The Express response object.
+ */
 exports.findOne = (req, res) => {
-    const id = req.params.id;
-    CheckOut.findByPk(id)
+  const id = req.params.id;
+  CheckOut.findByPk(id)
     .then((data) => {
-        res.send(data);
+      res.send(data);
     })
     .catch((e) => {
-        res.status(500).send({
-            message: "Could not find the data",
-        });
+      res.status(500).send({
+        message: "Could not find the data",
+      });
     });
 };
 
+/**
+ * Delete a checkout entry by its ID.
+ *
+ * @param {import('express').Request} req - The Express request object.
+ * @param {import('express').Response} res - The Express response object.
+ */
 exports.delete = (req, res) => {
-    const id = req.params.id;
-    CheckOut.destroy({
-      where: { id: id },
-    })
-      .then((num) => {
-        if (num == 1) {
-          res.send({
-            message: "CheckOut was deleted",
-          });
-        } else {
-          res.send({
-            message: "Could not delete",
-          });
-        }
-      })
-      .catch((e) => {
-        res.status(500).send({
-          message: "Database error",
+  const id = req.params.id;
+  CheckOut.destroy({
+    where: { id: id },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "CheckOut was deleted",
         });
+      } else {
+        res.send({
+          message: "Could not delete",
+        });
+      }
+    })
+    .catch((e) => {
+      res.status(500).send({
+        message: "Database error",
       });
-  };
-
-exports.update = (req, res) => {
-    const id = req.params.id;
-    CheckOut.update(req.body, {
-      where: { id: id },
-    })
-      .then((num) => {
-        if (num == 1) {
-          res.send({
-            message: "CheckOut was updatedd",
-          });
-        } else {
-          res.send({
-            message: "Could not update",
-          });
-        }
-      })
-      .catch((e) => {
-        res.status(500).send({
-          message: "Database error",
-        });
-      });
-  };
-
-
-  exports.findCheckOutByUser = (req, res) => {
-    const id = req.params.id;
-    CheckOut.findAll({ where: {userID: id}})
-    .then((data) => {
-        res.send(data);
-    })
-    .catch((e) =>{
-        res.status(500).send({
-            message: "Could not find the data",
-        });
     });
-  };
+};
+
+/**
+ * Update a checkout entry by its ID.
+ *
+ * @param {import('express').Request} req - The Express request object.
+ * @param {import('express').Response} res - The Express response object.
+ */
+exports.update = (req, res) => {
+  const id = req.params.id;
+  CheckOut.update(req.body, {
+    where: { id: id },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "CheckOut was updated",
+        });
+      } else {
+        res.send({
+          message: "Could not update",
+        });
+      }
+    })
+    .catch((e) => {
+      res.status(500).send({
+        message: "Database error",
+      });
+    });
+};
+
+/**
+ * Find and return all checkouts associated with a specific user.
+ *
+ * @param {import('express').Request} req - The Express request object.
+ * @param {import('express').Response} res - The Express response object.
+ */
+exports.findCheckOutByUser = (req, res) => {
+  const id = req.params.id;
+  CheckOut.findAll({ where: { userId: id } })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((e) => {
+      res.status(500).send({
+        message: "Could not find the data",
+      });
+    });
+};
--- back/app/controllers/product.controller.js
+++ back/app/controllers/product.controller.js
@@ -1,81 +1,108 @@
-const db = require("../models");
-const Product = db.products;
-const OP = db.Sequelize.Op;
+const db = require('../models')
+const Product = db.products
+const OP = db.Sequelize.Op
 
-exports.findAll = (req, res) =>{
-    Product.findAll().then((data) => {
-        res.send(data);
-    });
-};
-
-exports.create = (req, res) =>{
-    Product.create(req, body)
-    .then((data) => {
-        res.send(data);
+/**
+ * Find and return all products from the database.
+ *
+ * @param {import('express').Request} req - The Express request object.
+ * @param {import('express').Response} res - The Express response object.
+ */
+exports.findAll = (req, res) => {
+    Product.findAll()
+    .then(data => {
+        res.send(data)
     })
-    .catch((e) =>{
-        res.status(500).send({
-            message: "Could not insert into the table",
-        });
-    });
-};
-
+}
+/**
+ * Create a new product in the database.
+ *
+ * @param {import('express').Request} req - The Express request object.
+ * @param {import('express').Response} res - The Express response object.
+ */
+exports.create = (req, res) => {
+    //console.log(req.body)
+    Product.create(req.body)
+        .then(data => {
+            res.send(data)
+        })
+        .catch(e => {
+            res.status(500).send({
+                message: 'Could not insert into the table'
+            })
+        })
+}
+/**
+ * Find a product by its ID.
+ *
+ * @param {import('express').Request} req - The Express request object.
+ * @param {import('express').Response} res - The Express response object.
+ */
 exports.findOne = (req, res) => {
-    const id = req.params.id;
+    const id = req.params.id
     Product.findByPk(id)
-    .then((data) =>{
-        res.send(data);
+    .then(data => {
+        res.send(data)
     })
-    .catch((e) =>{
+    .catch(e => {
         res.status(500).send({
-            message: "Could not find the data",
-        });
-    });
-};
-
+            message: 'Could not find the data'
+        })
+    })
+}
+/**
+ * Delete a product by its ID.
+ *
+ * @param {import('express').Request} req - The Express request object.
+ * @param {import('express').Response} res - The Express response object.
+ */
 exports.delete = (req, res) => {
-    const id = req.params.id;
+    const id = req.params.id
     Product.destroy({
-        where : {id:id},
+        where: {id : id}
     })
-    .then((num) => {
-        if(num ==1){
+    .then( num => {
+        if(num == 1 ){
             res.send({
-                message: "Product was deleted",
-            });
-        } else{
+                message: 'Product was deleted'
+            })
+        }else {
             res.send({
-                message: "Could not delete",
-            });
+                message: "Could not delete"
+            })
         }
     })
-    .catch((e) => {
+    .catch(e => {
         res.status(500).send({
-            message: "Database error",
-        });
-    });
-};
-
+            message: 'Database error'
+        })
+    })
+}
+/**
+ * Update a product by its ID.
+ *
+ * @param {import('express').Request} req - The Express request object.
+ * @param {import('express').Response} res - The Express response object.
+ */
 exports.update = (req, res) => {
-    const id = req.params.id;
+    const id = req.params.id
     Product.update(req.body, {
-      where: { id: id },
+        where: {id: id}
     })
-      .then((num) => {
-        if (num == 1) {
-          res.send({
-            message: "Product was updatedd",
-          });
-        } else {
-          res.send({
-            message: "Could not update",
-          });
+    .then( num => {
+        if(num == 1 ){
+            res.send({
+                message: 'Product was updatedd'
+            })
+        }else {
+            res.send({
+                message: "Could not update"
+            })
         }
-      })
-      .catch((e) => {
+    })
+    .catch(e => {
         res.status(500).send({
-          message: "Database error",
-        });
-      });
-  };
-  
+            message: 'Database error'
+        })
+    })
+}
